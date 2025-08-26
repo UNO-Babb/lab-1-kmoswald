c19e0450e5959bc0ed7077df6eaf82f798a35389
kmoswald <kaitlynoswald@unomaha.edu>
2025-08-26 21:11:56 +0000

    Forgot to make it a story- now done with lab.

--- MadLib.py
+++ MadLib.py
@@ -14,10 +14,10 @@ noun2 = input("Enter a noun: ")
 verb3 = input("Enter a verb: ")
   #Print the story with the user supplied words.
 print("I like to ride a " + noun1 + ".")
-print("Yesterday I was " + verb1 + ".")
-print("The flowers are " + adjective1 + ".") 
-print("The flowers " + verb2 + " in the wind .")
-print("The " + noun2 + " is " + verb3 + " with their blocks .")
+print("Yesterday I was " + verb1 + " on a trail .")
+print("The flowers were " + adjective1 + ".") 
+print("The flowers were " + verb2 + " in the wind .")
+print("A " + noun2 + " is " + verb3 + " overhead .")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
